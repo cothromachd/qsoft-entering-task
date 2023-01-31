ffcf55d60e4f758450d8043edf438014b039c0e9
Khalid <dzhamaldaev@internet.ru>
2023-01-31 22:25:39 +0300

    Исправил нахождение кол-ва дней, добавил Makefile

--- main.go
+++ main.go
@@ -14,16 +14,39 @@ func main() {
 	router := gin.Default()
 
 	router.Any("/when/:year", func(ctx *gin.Context) {
+
+		// Получем year из запроса.
 		yearParam := ctx.Param("year")
-		yearInt, err := strconv.Atoi(yearParam)
-		yearTime := time.Date(yearInt, time.January, 1, 0, 0, 0, 0, time.UTC)
-		sinceYear := yearTime.Sub(time.Now().UTC())
+
+		// Кастуем из строки в целое число, чтобы
+		// вставить в параметры time.Date()
+		yearTypeInt, err := strconv.Atoi(yearParam)
 		if err != nil {
 			log.Println(err)
 		}	
+
+		// Образуем дату time.Time с заданнным year.
+		yearTypeTime := time.Date(yearTypeInt, time.January, 1, 0, 0, 0, 0, time.UTC)
+		
+		// Сохраняем только актуальный год, месяц и день, чтобы часы, минуты и.т.д
+		// не мешали исчислению прошедших дней.
+		Now := time.Now().UTC()
+		curTimestamp := time.Date(Now.Year(), Now.Month(), Now.Day(), 0, 0, 0, 0, time.UTC)
+		
+		// Находим разницу между заданной датой и настоящей датой.
+		sinceYear := yearTypeTime.Sub(curTimestamp)	
+		
+		// Находим количество прошедших/предстоящих дней от заданной даты.
+		// Для этого переведем часы в дни поделив часы на 24, так как
+		// в днях (имеется ввиду в сутках) 24 часа.
+		// В случае, если значение отрицательное - берём модуль числа.
 		daysResponse := int(math.Abs(sinceYear.Hours() / 24))
+		
+		// Отправляем ответ.
 		ctx.String(http.StatusOK, "%d", daysResponse)
+
 	})
 
+	// Запускаем сервер на порту по умолчанию.
 	router.Run()
 }
